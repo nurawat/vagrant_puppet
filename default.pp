8f596074e788751aa072957f6ca2a5613df2a631
=Arun Rawat <=rawatarun65@gmail.com>
2017-04-06 22:38:03 +0000

    New Puppet Script

---
+++ default.pp
@@ -0,0 +1,37 @@
+#create Directory
+file {'/home/projects':
+	ensure => directory,
+	owner => "vagrant",
+	group => "vagrant",
+	mode => "750", 
+	
+}
+
+
+#--------------------------------------------------------------------
+
+
+#installing things
+
+############################# Apache ######################################
+class{'apache':
+	
+}
+
+apache::vhost{'192.168.121.132':
+	port => '80',
+	docroot => '/var/www/',
+	ssl => true ,
+
+}
+
+
+##################################### MySQL #########################
+class { '::mysql::server':
+  root_password    => 'nojoke',
+  override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+}
+
+
+
+##########################################
